738a19336e13630e5e40d53cb3fa6a6a4687803e
Tobias Hauck <tobi1craft.mc@gmail.com>
2025-01-31 19:45:40 +0100

    some little window tweaks

--- lwjgl3/src/main/java/de/tobi1craft/rapidtrack/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/de/tobi1craft/rapidtrack/lwjgl3/Lwjgl3Launcher.java
@@ -23,16 +23,20 @@ public class Lwjgl3Launcher {
         configuration.setTitle("RapidTrack");
         //// Vsync limits the frames per second to what your hardware can display and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
+        configuration.useVsync(false);
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(1280, 720);
         //// You can change these files; they are in lwjgl3/src/main/resources/
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+
+        configuration.setPauseWhenMinimized(false);
+        //LostFocus also triggers when minimized
+        configuration.setPauseWhenLostFocus(true);
         return configuration;
     }
 }
